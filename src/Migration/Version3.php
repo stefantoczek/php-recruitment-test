fe5b98a880391b9e241425c47b6dbd2fc6188626
Stefan Toczek <stefan.toczek@gate-software.com>
2019-03-12 14:19:50 +0100

    task 3 - added tracking of last visits on pages

---
+++ src/Migration/Version3.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Snowdog\DevTest\Migration;
+
+use Snowdog\DevTest\Core\Database;
+use Snowdog\DevTest\Model\PageManager;
+use Snowdog\DevTest\Model\UserManager;
+use Snowdog\DevTest\Model\WebsiteManager;
+
+class Version3
+{
+    /**
+     * @var Database|\PDO
+     */
+    private $database;
+    /**
+     * @var UserManager
+     */
+    private $userManager;
+    /**
+     * @var WebsiteManager
+     */
+    private $websiteManager;
+    /**
+     * @var PageManager
+     */
+    private $pageManager;
+
+    public function __construct(
+        Database $database,
+        UserManager $userManager,
+        WebsiteManager $websiteManager,
+        PageManager $pageManager
+    ) {
+        $this->database = $database;
+        $this->userManager = $userManager;
+        $this->websiteManager = $websiteManager;
+        $this->pageManager = $pageManager;
+    }
+
+    public function __invoke()
+    {
+        $this->updatePageTable();
+    }
+
+    private function updatePageTable()
+    {
+        $updateQuery = <<<SQL
+ALTER TABLE `pages`
+ADD `last_visited` int(11) unsigned;
+SQL;
+        $this->database->exec($updateQuery);
+    }
+
+}
